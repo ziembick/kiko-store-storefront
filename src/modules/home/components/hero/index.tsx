d89d316c6f50497dd9c4673d5e0806b698028750
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-22 20:59:17 -0300

    pq nao vai

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -6,9 +6,7 @@ const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        {/* <Image src="/banner_main.jpg" alt="Main banenr" width={1440} height={100}/> */}
-
-        testetstestetstestete
+        <Image src="./banner_main.jpg" alt="Main banenr" width={1440} height={100}/>
       </div>
     </div>
   )
