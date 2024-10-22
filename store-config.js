718dc13f7c20dae1d3bad4a1b853e14514396cba
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-21 21:58:20 -0300

    feat: angolia

--- store-config.js
+++ store-config.js
@@ -1,9 +1,29 @@
+// function withStoreConfig(nextConfig = {}) {
+//   const features = nextConfig.features || {}
+//   delete nextConfig.features
+
+//   nextConfig.env = nextConfig.env || {}
+
+//   Object.entries(features).forEach(([key, value]) => {
+//     if (value) {
+//       nextConfig.env[`FEATURE_${key.toUpperCase()}_ENABLED`] = true
+//     }
+//   })
+
+//   return nextConfig
+// }
+
+// module.exports = { withStoreConfig }
+
+
+// store-config.js
 function withStoreConfig(nextConfig = {}) {
   const features = nextConfig.features || {}
   delete nextConfig.features
 
   nextConfig.env = nextConfig.env || {}
 
+  // Loop through the features and set environment variables for each
   Object.entries(features).forEach(([key, value]) => {
     if (value) {
       nextConfig.env[`FEATURE_${key.toUpperCase()}_ENABLED`] = true
@@ -13,4 +33,10 @@ function withStoreConfig(nextConfig = {}) {
   return nextConfig
 }
 
-module.exports = { withStoreConfig }
+// Export your config with the search feature enabled
+module.exports = withStoreConfig({
+  features: {
+    search: true
+  }
+})
+
