391ad7adbac284afc16aa372b11fb9d100d3ea75
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-21 22:03:37 -0300

    fix

--- store-config.js
+++ store-config.js
@@ -1,29 +1,9 @@
-// function withStoreConfig(nextConfig = {}) {
-//   const features = nextConfig.features || {}
-//   delete nextConfig.features
-
-//   nextConfig.env = nextConfig.env || {}
-
-//   Object.entries(features).forEach(([key, value]) => {
-//     if (value) {
-//       nextConfig.env[`FEATURE_${key.toUpperCase()}_ENABLED`] = true
-//     }
-//   })
-
-//   return nextConfig
-// }
-
-// module.exports = { withStoreConfig }
-
-
-// store-config.js
 function withStoreConfig(nextConfig = {}) {
   const features = nextConfig.features || {}
   delete nextConfig.features
 
   nextConfig.env = nextConfig.env || {}
 
-  // Loop through the features and set environment variables for each
   Object.entries(features).forEach(([key, value]) => {
     if (value) {
       nextConfig.env[`FEATURE_${key.toUpperCase()}_ENABLED`] = true
@@ -33,10 +13,4 @@ function withStoreConfig(nextConfig = {}) {
   return nextConfig
 }
 
-// Export your config with the search feature enabled
-module.exports = withStoreConfig({
-  features: {
-    search: true
-  }
-})
-
+module.exports = { withStoreConfig }
