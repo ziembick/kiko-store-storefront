a6c428b79c006692fe6c9941fcf578439dd068af
Paulo Ziembick <pauloziembick@gmail.com>
2024-10-22 21:08:22 -0300

    nova img

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,14 +1,34 @@
-// import { Github } from "@medusajs/icons"
-// import { Button, Heading } from "@medusajs/ui"
+import { Github } from "@medusajs/icons"
+import { Button, Heading } from "@medusajs/ui"
 import Image from "next/image"
 
 const Hero = () => {
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <Image src="/banner_main.jpg" alt="Main banenr" width={1440} height={100}/>
-        <Image src="/banner_main.jpg" alt="Main banenr2"/>
-        <Image src="/img/banner2.jpg" alt="Main banenr3"/>
+        <span>
+          <Heading
+            level="h1"
+            className="text-3xl leading-10 text-ui-fg-base font-normal"
+          >
+            <Image src="/banner_main.jpg" alt="Image banner" width={1440} height={100}/>
+          </Heading>
+          {/* <Heading
+            level="h2"
+            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+          >
+            Powered by Medusa and Next.js
+          </Heading> */}
+        </span>
+        <a
+          href="https://github.com/medusajs/nextjs-starter-medusa"
+          target="_blank"
+        >
+          <Button variant="secondary">
+            View on GitHub
+            <Github />
+          </Button>
+        </a>
       </div>
     </div>
   )
