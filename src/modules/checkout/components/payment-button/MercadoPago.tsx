02672404371aabdbccc5f396608b21fa36fdbb8c
Paulo Ziembick <pauloziembick@gmail.com>
2024-11-11 22:10:14 -0300

    só coloquei o mercado pago tsx

--- src/modules/checkout/components/payment-button/MercadoPago.tsx
+++ src/modules/checkout/components/payment-button/MercadoPago.tsx
@@ -1,25 +1,27 @@
-// import { PaymentSession } from "@medusajs/medusa";
-// import { Button } from "@medusajs/ui";
-// import { useMercadopago } from "react-sdk-mercadopago";
+import { PaymentSession } from "@medusajs/medusa";
+import { Button } from "@medusajs/ui";
+import { useMercadopago } from "react-sdk-mercadopago";
 
-// const MERCADOPAGO_PUBLIC_KEY = process.env.NEXT_PUBLIC_MERCADOPAGO_PUBLIC_KEY || "";
+const MERCADOPAGO_PUBLIC_KEY = process.env.NEXT_PUBLIC_MERCADOPAGO_PUBLIC_KEY || "";
 
-// const MercadoPagoButton = ({ session }: { session: PaymentSession }) => {
-//   const mercadoPago = useMercadopago.v2(MERCADOPAGO_PUBLIC_KEY, {
-//     locale: "es-PE",
-//   });
+const MercadoPagoButton = ({ session }: { session: PaymentSession }) => {
+  const mercadoPago = useMercadopago.v2(MERCADOPAGO_PUBLIC_KEY, {
+    locale: "pt-BR",
+  });
 
-//   const checkout = mercadoPago?.checkout({
-//     preference: {
-//       id: session.data.preferenceId, //preference ID
-//     },
-//   });
+  const checkout = mercadoPago?.checkout({
+    preference: {
+      id: session.data.preferenceId, //preference ID
+    },
+  });
 
-//   return (
-//     <Button
-//       onClick={() => checkout.open()}
-//     >
-//       Pagar
-//     </Button>
-//   );
-// };
+  return (
+    <Button
+      onClick={() => checkout.open()}
+    >
+      Pagar
+    </Button>
+  );
+};
+
+export default MercadoPagoButton
