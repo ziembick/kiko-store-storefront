bacb8a5c1b584200fd5420e6c70bfe708f07f8d3
Paulo Ziembick <pauloziembick@gmail.com>
2024-11-11 21:54:01 -0300

    voltando ao normal

--- src/modules/checkout/components/payment-button/MercadoPago.tsx
+++ src/modules/checkout/components/payment-button/MercadoPago.tsx
@@ -1,45 +1,25 @@
-// MercadoPagoButton.tsx
-"use client";
-
-import { useMercadopago } from "react-sdk-mercadopago";
+import { PaymentSession } from "@medusajs/medusa";
 import { Button } from "@medusajs/ui";
-import React, { useEffect } from "react";
+import { useMercadopago } from "react-sdk-mercadopago";
 
 const MERCADOPAGO_PUBLIC_KEY = process.env.NEXT_PUBLIC_MERCADOPAGO_PUBLIC_KEY || "";
 
-interface MercadoPagoButtonProps {
-  preferenceId: string;
-}
-
-const MercadoPagoButton: React.FC<MercadoPagoButtonProps> = ({ preferenceId }) => {
-  const mercadopago = useMercadopago.v2(MERCADOPAGO_PUBLIC_KEY, {
-    locale: "pt-BR",
+const MercadoPagoButton = ({ session }: { session: PaymentSession }) => {
+  const mercadoPago = useMercadopago.v2(MERCADOPAGO_PUBLIC_KEY, {
+    locale: "es-PE",
   });
 
-  useEffect(() => {
-    if (mercadopago) {
-      mercadopago.checkout({
-        preference: {
-          id: preferenceId,
-        },
-      });
-    }
-  }, [mercadopago, preferenceId]);
+  const checkout = mercadoPago?.checkout({
+    preference: {
+      id: session.data.preferenceId, //preference ID
+    },
+  });
 
   return (
     <Button
-      onClick={() => {
-        try {
-          mercadopago?.checkout({ preference: { id: preferenceId } }).open();
-        } catch (error) {
-          console.error("Erro ao abrir o checkout:", error);
-          alert("Ocorreu um erro ao iniciar o pagamento. Tente novamente.");
-        }
-      }}
+      onClick={() => checkout.open()}
     >
-      Pagar com Mercado Pago
+      Pagar
     </Button>
   );
 };
-
-export default MercadoPagoButton;
